50a8c1e22de46383e1ef8f04ad2cf79998d1557e
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-22 16:25:27 +0700

    adding like button

--- actions/posts.js
+++ actions/posts.js
@@ -1,8 +1,10 @@
 "use server";
 
-import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -11,35 +13,43 @@ export async function createPost(prevState, formData) {
 
   let errors = [];
 
-  if (!title && title.trim().length === 0) {
-    errors.push("Title is required");
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
   }
-  if (!content && content.trim().length === 0) {
-    errors.push("Content is required");
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
   }
+
   if (!image || image.size === 0) {
-    errors.push("Image is required");
+    errors.push("Image is required.");
   }
 
   if (errors.length > 0) {
     return { errors };
   }
 
-  let imageUrl = "";
+  let imageUrl;
+
   try {
     imageUrl = await uploadImage(image);
   } catch (error) {
     throw new Error(
-      "Failed to upload image, post not created. Please try again."
+      "Image upload failed, post was not created. Please try again later."
     );
   }
 
   await storePost({
-    imageUrl,
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath('/', 'layout');
 }
