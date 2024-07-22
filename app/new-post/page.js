484df845c704cf2d80c5ba238898bdbf577d2d81
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-22 16:07:54 +0700

    adding cloudinary

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,39 +1,6 @@
+import { createPost } from "@/actions/post";
 import PostForm from "@/components/post-form";
-import { storePost } from "@/lib/posts";
-import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    let errors = [];
-
-    if (!title && title.trim().length === 0) {
-      errors.push("Title is required");
-    }
-    if (!content && content.trim().length === 0) {
-      errors.push("Content is required");
-    }
-    if (!image) {
-      errors.push("Image is required");
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    redirect("/feed");
-  }
-
   return <PostForm action={createPost} />;
 }
