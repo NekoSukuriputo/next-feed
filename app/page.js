cd65c5847b2734db5d396c2e1e637d35378ca51e
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-26 15:56:09 +0700

    metadata setup

--- app/page.js
+++ app/page.js
@@ -3,6 +3,20 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'NextPosts',
+//   decription: 'Browse and share amazing posts.',  
+// } static meta data
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts`,
+    description: 'Browse and share amazing posts.',
+  };
+} // dynamic meta data
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
